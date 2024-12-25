5613e8fffd0888ffb4110c0ec5f9903609fd0d29
unknown <shimaafathi126@gmail.com>
2024-12-25 14:20:09 +0200

    installation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="bg-black">
+    <h1 className="text-3xl font-bold text-white underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
